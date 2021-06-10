9e798c3e769d7b01cb8a4d2dedc1a3c1c2d05c49
Miguel M Álvarez <miguelmartin.alvarez@irbbarcelona.org>
2021-06-10 13:04:35 +0200

    add raw data paths & other major changes

--- run_analysis.R
+++ run_analysis.R
@@ -1,182 +1,196 @@
+
 ### load libraries
+library(magrittr)
 library(vroom)
+library(dplyr)
+library(tidyr)
 library(tibble)
-library(Rsamtools)
-library(GenomicRanges)
-library(DESeq2)
-library(lme4)
-library(lmerTest)
-library(multcomp)
+library(stringr)
+library(readr)
+library(writexl)
 library(ggplot2)
-library(mda)
-library(gplots)
+library(MASS)
+# library(Rsamtools)
+# library(GenomicRanges)
+library(DESeq2)
+# library(multcomp)
+# library(mda)
+# library(gplots)
+library(conflicted)
+
+# avoid function conflicts
+conflict_prefer("filter", "dplyr")
+conflict_prefer("rename", "dplyr")
+conflict_prefer("select", "dplyr")
+conflict_prefer("extract", "magrittr")
+conflict_prefer("Position", "ggplot2")
 
 ## load regression custom functions
 source("src/functions.R")
 
 
-## define directory paths
-mainDir <- $PWD
-dataDir <- paste(mainDir, 'data/raw/', sep='')
-dataProcDir <- paste(mainDir, 'data/processed/', sep='')
-repDir <- paste(mainDir, 'output/', sep='')
-tabDir <- paste(repDir, 'tables/', sep='')
-figDir <- paste(repDir, 'figs/', sep='')
+
+## define and create directories
+
+# project directory
+mainDir = getwd()
+
+# processed data directory
+dataDir = paste0(mainDir, 'data/processed/') ; dir.create(dataDir, recursive = TRUE)
+
+# output directories
+outDir = paste0(mainDir, 'output/')
+tabDir = paste0(outDir, 'tables/') ; dir.create(tabDir, recursive = TRUE)
+figDir = paste0(outDir, 'figs/') ; dir.create(figDir, recursive = TRUE)
+
 # temporary directory
-tmpDir <- "/local_scratch/malvarez/chrysper/analysis/"
+tmpDir = "/local_scratch/malvarez/chrysper/tmp/"
 
 
 
-### load info on samples
-sampleinfo <- read.table(paste0(dataDir, "sampleinfo.txt"), sep="\t", header=T, stringsAsFactors=F)[,1:10]
-sampleinfo$filename <- paste0(dataDir, "fastas/180329/0", sampleinfo$FGC.Pool.ID, "_", sampleinfo$CRG.Sample.ID, "_", substr(sampleinfo$barcode.seq, 1, 7), ".fastq.gz")
-colnames(sampleinfo)[colnames(sampleinfo) == "group"] <- "sampleName"
-sampleinfo$group <- paste0(c(rep("tumor", 12), "tcbt", "tcbt", "cu", "cdt", "cdr.res"), "_", gsub(" ", "", sampleinfo$treatment))
+#### load data
+
+# raw data's root path
+rawPath = "/g/strcombio/fsupek_data/CRISPR/"
 
 
-### load info on guides
-f1 <- paste0(dataDir, "broadgpp-brunello-library-contents.txt")
-gl <- read.table(f1, stringsAsFactors=F, sep="\t", header=T)
-rownames(gl) <- paste0(gl[,2], "-", 1:nrow(gl))
+### load tables of raw read counts (3 different experiments)
 
-
-### load matrix of raw read counts
-mat = vroom(paste0(dataDir, "mat.counts.csv")) %>%
-  column_to_rownames("sgRNA") %>%
-  # all counts are 0 on column cdt, dropped from the analysis
-  select(-cdt) %>%
+## our APOBEC3A experiment
+# cell lines --> A549, LXF289, H358
+# treatments --> DOX (A3A active) vs. control -- some also have ATRi
+# altered genotypes --> TP53-KO, TP53-mut, HMCES-KO
+A3A_Dir = paste0(rawPath, "2_processed_data/apobec/MAGeCK_MLE_analyses/raw_counts_combined/")
+A3A_raw_counts = vroom(paste0(A3A_Dir, "A549_LXF289_H358__raw_counts.tsv")) %>%
   as.matrix()
+# load sample info 
+A3A_sampleinfo = vroom(paste0(A3A_Dir, "sampleinfo.tsv"))
+# more info
+#/g/strcombio/fsupek_data/CRISPR/1_raw_data/0_INFO/paths_info_A549_LXF289_H358.xlsx
+
+## Travis' ATRi/TMZ experiment
+# cell lines --> SW480, SW620, HT29
+# treatments --> ATRi, TMZ vs. control
+# altered genotypes --> ARID1A-KO, MSH6-KO
+ATRi_TMZ_Dir = paste0(rawPath, "/g/strcombio/fsupek_data/CRISPR/3_reports/stracker_lab/isabel_ATRi_TMZ/raw_counts_combined/")
+ATRi_TMZ_raw_counts = vroom(paste0(ATRi_TMZ_Dir, "SW480_SW620_HT29__ARID1A_MSH6__raw_counts.tsv")) %>%
+  as.matrix()
+# load sample info 
+ATRi_TMZ_sampleinfo = vroom(paste0(ATRi_TMZ_Dir, "sampleinfo.tsv"))
+# more info
+#readLines("/g/strcombio/fsupek_data/CRISPR/1_raw_data/0_INFO/paths_info_isabel.txt")
+
+## Jain et al oxygen experiment
+# cell lines --> K562
+# treatment --> oxygen concentration 21%, 5%, 1%
+oxygen_Dir = paste0(rawPath, "4_resources/public_CRISPR_raw_counts/K562_oxygen_jain/")
+oxygen_raw_counts = vroom(paste0(oxygen_Dir, "raw_counts.tsv")) %>%
+  as.matrix()
+# load sample info 
+oxygen_sampleinfo = vroom(paste0(oxygen_Dir, "sampleinfo.tsv"))
+# more info
+#readLines(paste0(oxygen_Dir, "README"))
 
 
-### normalize count matrix (3 ways)
+### load sgRNAs library (Brunello)
+grna_library = vroom(paste0(rawPath, "4_resources/crispr_libraries/brunello/original/broadgpp-brunello-library-contents_gRNAs.tsv")) %>%
+  rename("gene" = "Target Gene Symbol") %>%
+  select(sgRNA, gene, "sgRNA Target Sequence", "Target Context Sequence", "PAM Sequence", "Strand", "Genomic Sequence", "Position of Base After Cut (1-based)") %>%
+  arrange(`Genomic Sequence`, `Position of Base After Cut (1-based)`)
 
-## rlog
-rlog = rlog(mat)
+
+
+### normalize count matrix 
+
+## working only with APOBEC3A data for the moment
 
 ## varianceStabilizingTransformation
-vst = varianceStabilizingTransformation(mat)
-
-## log2-normalized-reads-per-million for each sgRNA
-norm = log2( ( t( t(mat) / colSums(mat) ) * 1e6 ) + 1)
-# another version (Camille's)
-norm2 = log2( t( t(mat + 1) / colSums(mat + 1) ) * 1e7 )
+vst = varianceStabilizingTransformation(A3A_raw_counts)
 
 # print normalized sample distributions
 pdf(paste0(figDir, "normalization.pdf"), width=15, height=6)
-par(mfrow=c(1,3))
+par(mfrow=c(1,1))
 boxplot(vst, outline=F, las=2, main="vst")
-boxplot(rlog, outline=F, las=2, main="rlog")
-boxplot(norm, outline=F, las=2, main="shift log")
-boxplot(norm2, outline=F, las=2, main="shift log Camille")
 dev.off()
 
 
 ## create Biobase's 'ExpressionSets' (esets)
-esets <- list()
+eset = new("ExpressionSet", exprs=vst)
+pData(eset) = sampleinfo[match(colnames(eset), sampleinfo$sampleName),]
+eset$group = factor(eset$group)
+rownames(eset) = rownames(mat)
+fData(eset) = grna_library[rownames(eset), ]
 
-eset <- new("ExpressionSet", exprs=rlog)
-pData(eset) <- sampleinfo[match(colnames(eset), sampleinfo$sampleName),]
-eset$group <- factor(eset$group)
-rownames(eset) <- rownames(mat)
-fData(eset) <- gl[rownames(eset), ]
-esets[["rlog"]] <- eset
-
-eset <- new("ExpressionSet", exprs=vst)
-pData(eset) <- sampleinfo[match(colnames(eset), sampleinfo$sampleName),]
-eset$group <- factor(eset$group)
-rownames(eset) <- rownames(mat)
-fData(eset) <- gl[rownames(eset), ]
-esets[["vst"]] <- eset
-
-eset <- new("ExpressionSet", exprs=norm)
-pData(eset) <- sampleinfo[match(colnames(eset), sampleinfo$sampleName),]
-eset$group <- factor(eset$group)
-rownames(eset) <- rownames(mat)
-fData(eset) <- gl[rownames(eset), ]
-esets[["norm"]] <- eset
-
-eset <- new("ExpressionSet", exprs=norm2)
-pData(eset) <- sampleinfo[match(colnames(eset), sampleinfo$sampleName),]
-eset$group <- factor(eset$group)
-rownames(eset) <- rownames(mat)
-fData(eset) <- gl[rownames(eset), ]
-esets[["norm2"]] <- eset
-
-save(esets, file=paste0(dataDir, "esets.RData"))
-#load(file=paste0(dataDir, "esets.RData"))
+save(eset, file=paste0(dataDir, "eset.RData"))
+#load(file=paste0(dataDir, "eset.RData"))
 
 
 ## PCAs
-for(i in names(esets)){
-  pr <- prcomp(t(exprs(esets[[i]])))
-  pcvar <- round(pr$sdev^2/sum(pr$sdev^2)*100, 2)
-  ##
-  pdf(paste0(figDir, "pca.", i, ".pdf"), width=9, height=9)
-  par(mfrow=c(3, 3))
-  for(i in 2:3){
-    plot(pr$x[,1], pr$x[,i], col=rainbow(5)[esets[[i]]$group], xlab=paste0("PC1 (", pcvar[1], "%)"), ylab=paste0("PC", i, " (", pcvar[i], "%)"))
-    text(pr$x[,1], pr$x[,i], labels=esets[[i]]$sampleName, pos=4, cex=0.6, col=rainbow(5)[esets[[i]]$group], srt=90)
-  }
-  plot(pr$x[,1], apply(exprs(esets[[i]]), 2, median), col=rainbow(5)[esets[[i]]$group], xlab=paste0("PC1 (", pcvar[1], "%)"), ylab="Col medians")
-  plot(pr$x[,1], colSums(mat), col=rainbow(5)[esets[[i]]$group], xlab=paste0("PC1 (", pcvar[1], "%)"), ylab="Col sums")
-  plot(pr$x[,2], colSums(mat), col=rainbow(5)[esets[[i]]$group], xlab=paste0("PC2 (", pcvar[2], "%)"), ylab="Col sums")
-  plot(pr$x[,3], colSums(mat), col=rainbow(5)[esets[[i]]$group], xlab=paste0("PC3 (", pcvar[3], "%)"), ylab="Col sums")
-  plot(pr$x[,1], colMeans(mat == 0), col=rainbow(5)[esets[[i]]$group], xlab=paste0("PC1 (", pcvar[1], "%)"), ylab="% zeros")
-  plot(pr$x[,2], colMeans(mat), col=rainbow(5)[esets[[i]]$group], xlab=paste0("PC2 (", pcvar[2], "%)"), ylab="% zeros")
-  plot(pr$x[,3], colMeans(mat), col=rainbow(5)[esets[[i]]$group], xlab=paste0("PC3 (", pcvar[3], "%)"), ylab="% zeros")
-  dev.off()
+pr = prcomp(t(exprs(eset)))
+pcvar = round(pr$sdev^2/sum(pr$sdev^2)*100, 2)
+
+pdf(paste0(figDir, "pca.", i, ".pdf"), width=9, height=9)
+par(mfrow=c(3, 3))
+for(i in 2:3){
+  plot(pr$x[,1], pr$x[,i], col=rainbow(5)[eset$group], xlab=paste0("PC1 (", pcvar[1], "%)"), ylab=paste0("PC", i, " (", pcvar[i], "%)"))
+  text(pr$x[,1], pr$x[,i], labels=eset$sampleName, pos=4, cex=0.6, col=rainbow(5)[eset$group], srt=90)
 }
+plot(pr$x[,1], apply(exprs(eset), 2, median), col=rainbow(5)[eset$group], xlab=paste0("PC1 (", pcvar[1], "%)"), ylab="Col medians")
+plot(pr$x[,1], colSums(mat), col=rainbow(5)[eset$group], xlab=paste0("PC1 (", pcvar[1], "%)"), ylab="Col sums")
+plot(pr$x[,2], colSums(mat), col=rainbow(5)[eset$group], xlab=paste0("PC2 (", pcvar[2], "%)"), ylab="Col sums")
+plot(pr$x[,3], colSums(mat), col=rainbow(5)[eset$group], xlab=paste0("PC3 (", pcvar[3], "%)"), ylab="Col sums")
+plot(pr$x[,1], colMeans(mat == 0), col=rainbow(5)[eset$group], xlab=paste0("PC1 (", pcvar[1], "%)"), ylab="% zeros")
+plot(pr$x[,2], colMeans(mat), col=rainbow(5)[eset$group], xlab=paste0("PC2 (", pcvar[2], "%)"), ylab="% zeros")
+plot(pr$x[,3], colMeans(mat), col=rainbow(5)[eset$group], xlab=paste0("PC3 (", pcvar[3], "%)"), ylab="% zeros")
+dev.off()
 
 
 
 ### differential lethality
 
-### Work with VST-normalized counts
-eset <- esets[["vst"]]
-sampleNames(eset) <- eset$sampleName
-eset$mouse[13:16] <- 10:13
-colnames(fData(eset))[2] <- "gene"
-eset <- eset[!is.na(fData(eset)$gene),]
-eset$treatment[13:14] <- "NA"; eset$treatment[16] <- "drug treatment"; eset$treatment <- as.factor(eset$treatment)
+sampleNames(eset) = eset$sampleName
+eset$mouse[13:16] = 10:13
+colnames(fData(eset))[2] = "gene"
+eset = eset[!is.na(fData(eset)$gene),]
+eset$treatment[13:14] = "NA"; eset$treatment[16] = "drug treatment"; eset$treatment = as.factor(eset$treatment)
 
+# contrasts
 conts =  mcp(group = c("tumor_untreated - tcbt_NA = 0",
                        "tumor_drugtreatment - tcbt_NA = 0",
                        "tumor_drugtreatment - tumor_untreated = 0"))
 
-
-## run regression (loop across gene names)
-allres <- mclapply(unique(fData(eset)$gene), function(g){
-  df <- buildDF(g, eset, ref="tcbt_NA", cond="group")
-  y <- fitModel(df, conts, cond="group")
-  m1 <- tapply(df$counts, df$group, mean)
+## run NB regression (loop across gene names)
+allres = mclapply(unique(fData(eset)$gene), function(g){
+  df = buildDF(g, eset, ref="tcbt_NA", cond="group")
+  y = fitModel(df, conts, cond="group")
+  m1 = tapply(df$counts, df$group, mean)
   list(df=df, res=y, mns=m1)
 }, mc.cores=30)
-names(allres) <- unique(fData(eset)$gene)
+names(allres) = unique(fData(eset)$gene)
 
 save(allres, file=paste0(dataDir, "allres.RData"))
 
-
 ## parse results
-cres <- lapply(1:length(conts$group), function(i){
-  y <- do.call(rbind, lapply(allres, function(x) as.matrix(unname(x$res[i,, drop=F]))))
-  colnames(y) <- colnames(allres[[1]]$res)
-  mns <- do.call(rbind, lapply(allres, "[[", "mns"))
-  y <- cbind(y, mns)
-  rownames(y) <- names(allres)
+cres = lapply(1:length(conts$group), function(i){
+  y = do.call(rbind, lapply(allres, function(x) as.matrix(unname(x$res[i,, drop=F]))))
+  colnames(y) = colnames(allres[[1]]$res)
+  mns = do.call(rbind, lapply(allres, "[[", "mns"))
+  y = cbind(y, mns)
+  rownames(y) = names(allres)
   y
 })
-names(cres) <- rownames(allres[[1]]$res)
+names(cres) = rownames(allres[[1]]$res)
 
-cres <- lapply(cres, function(x) {
-  x <- data.frame(x)
-  x$pval.adj <- p.adjust(x$pvals, method="BH")
-  colnames(x) <- sub("ci.", "", colnames(x))
+cres = lapply(cres, function(x) {
+  x = data.frame(x)
+  x$pval.adj = p.adjust(x$pvals, method="BH")
+  colnames(x) = sub("ci.", "", colnames(x))
   x
 })
 
-mns <- cres[[1]][,5:9]
+mns = cres[[1]][,5:9]
 
-allres <- cbind(mns, untrVsTCBT = cres[[1]][, c(1:4, 10)], treatVsTCBT = cres[[2]][, c(1:4, 10)], treatVsuntr = cres[[3]][, c(1:4, 10)])
+allres = cbind(mns, untrVsTCBT = cres[[1]][, c(1:4, 10)], treatVsTCBT = cres[[2]][, c(1:4, 10)], treatVsuntr = cres[[3]][, c(1:4, 10)])
 
 # write tables
 write.table(allres, file=paste0(tabDir, "results.xls"), quote=F, row.names=T, sep="\t")
@@ -185,14 +199,14 @@ for(i in names(cres)) write.table(cres[[i]], file=paste0(tabDir, i, ".results.xl
 
 
 ## detect top 50 genes -sorted by max(|estimate|)- with significant regression (|estimate|>4 and FDR<0.05)
-sel <- unique(unlist(lapply(cres, function(x){
-  x <- x[order(abs(x$Estimate), decreasing=T),]
-  x <- x[x$pval.adj < 0.05 & abs(x$Estimate) > 4 ,]
+sel = unique(unlist(lapply(cres, function(x){
+  x = x[order(abs(x$Estimate), decreasing=T),]
+  x = x[x$pval.adj < 0.05 & abs(x$Estimate) > 4 ,]
   rownames(x)[1:min(nrow(x), 50)]
 })))
 
-mycols <- c(1, '#ffffe5','#fff7bc','#fee391','#fec44f','#fe9929','#ec7014','#cc4c02','#993404','#662506')
-mycols5 <- mycols[c(1, 3, 5, 7, 9)]
+mycols = c(1, '#ffffe5','#fff7bc','#fee391','#fec44f','#fe9929','#ec7014','#cc4c02','#993404','#662506')
+mycols5 = mycols[c(1, 3, 5, 7, 9)]
 
 system(paste0("mkdir ", figDir, "Stripcharts"))
 
@@ -202,7 +216,7 @@ system(paste0("mkdir ", figDir, "Stripcharts"))
 # 2 barplots per gene
 for(i in sel){
   
-  y <- buildDF(i, eset, ref="tcbt_NA")
+  y = buildDF(i, eset, ref="tcbt_NA")
   
   pdf(paste0(figDir, "Stripcharts/", i, ".grna.pdf"), width=10, height=8)
   q = ggplot(data = y,
--- src/functions.R
+++ src/functions.R
@@ -1,37 +1,21 @@
-### Functions
 
-buildDF <- function(g, eset, ref, cond="group"){
-    x <- eset[fData(eset)$gene == g,]
-    df <- data.frame(counts=as.vector(exprs(x)), gRNA=rep(rownames(x), ncol(x)), sample=rep(colnames(x), rep(nrow(x), ncol(x))),  stringsAsFactors=F)
-    df[, cond] <- pData(x)[df$sample, cond]
-    df$mouse <- pData(x)[df$sample,]$mouse
-    df$gene <- g
-    df[, cond] <- relevel(df[, cond], ref=ref)
+buildDF = function(g, eset, ref, cond="group"){
+    x = eset[fData(eset)$gene == g,]
+    df = data.frame(counts=as.vector(exprs(x)), gRNA=rep(rownames(x), ncol(x)), sample=rep(colnames(x), rep(nrow(x), ncol(x))),  stringsAsFactors=F)
+    df[, cond] = pData(x)[df$sample, cond]
+    df$mouse = pData(x)[df$sample,]$mouse
+    df$gene = g
+    df[, cond] = relevel(df[, cond], ref=ref)
     df
 }
 
-fitModel <- function(df, conts, cond="group"){
-    df$group <- df[, cond]
-    if(length(unique(df$gRNA)) > 1){
-        lm <- lmer(counts ~ group + (1 | gRNA) + (1|mouse/sample), data=df)
-        gl <- glht(lm, linfct=conts)
-        sm <- summary(gl, test=adjusted("none"))
-        pvals <- as.numeric(sm$test$pvalues)
-        ci <- as.data.frame(confint(sm)$confint)
-    }
-    else {
-        y <- lapply(conts$group, function(i){
-            ref <- sub(".*.- ", "", sub(" = 0", "", i))
-            sam <- sub(" -.*.", "", i)
-            df$group <- relevel(df$group, ref =ref)
-            lm <- lm(counts ~ group, data=df)
-            sm <- summary(lm)
-            pvals <- coef(sm)[paste0("group", sam),4]
-            ci <- data.frame(Estimate=coef(sm)[paste0("group", sam), 1], confint(lm)[paste0("group", sam),, drop=F])
-            list(ci=ci, pvals=pvals)
-        })
-        pvals <- do.call(rbind, lapply(y,"[[", "pvals")); rownames(pvals) <- conts$group
-        ci <- do.call(rbind, lapply(y,"[[", "ci")); rownames(ci) <- conts$group
-    }
+
+fitModel = function(df, conts, cond="group"){
+    df$group = df[, cond]
+    lm = lmer(counts ~ group + (1 | gRNA) + (1|mouse/sample), data=df)
+    gl = glht(lm, linfct=conts)
+    sm = summary(gl, test=adjusted("none"))
+    pvals = as.numeric(sm$test$pvalues)
+    ci = as.data.frame(confint(sm)$confint)
     data.frame(ci=ci, pvals=pvals)
 }
